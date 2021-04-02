7faf6f57b7fc223e39a1f9af34be2d1c014fb8ed
kromilka <akrilka2003@gmail.com>
2021-04-02 11:27:20 +1000

    Trigger Red Cloud

---
+++ Assets/Scripts/Quests/NegCloud.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NegCloud : MonoBehaviour
+{
+    public Animator[] clouds;
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            foreach (Animator anim in clouds)
+            {
+                anim.SetTrigger("Shit");
+            }
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            foreach (Animator anim in clouds)
+            {
+                anim.SetTrigger("Shit");
+            }
+        }
+    }
+}
